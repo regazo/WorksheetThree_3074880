4c24f81e6e1debac23c1fd67fc90b9038c5b434d
regazo <z.mbame11@gmail.com>
2024-03-13 21:56:12 +0000

    shape integration class

---
+++ WorksheetThree_3074880/src/main/java/Shapes/Shape.java
@@ -0,0 +1,25 @@
+package Shapes;
+
+public abstract class Shape {
+    private String name;
+
+    public Shape(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public abstract double area();
+    public abstract double perimeter();
+
+    @Override
+    public String toString() {
+        return "Shape: " + name;
+    }
+}
