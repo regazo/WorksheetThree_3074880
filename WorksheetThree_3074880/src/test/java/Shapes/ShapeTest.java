ef25397057461ad9cb623b5163424e4d160635f6
regazo <z.mbame11@gmail.com>
2024-03-13 23:46:55 +0000

    added unit tests for rhombus area

--- WorksheetThree_3074880/src/test/java/Shapes/ShapeTest.java
+++ WorksheetThree_3074880/src/test/java/Shapes/ShapeTest.java
@@ -14,5 +14,12 @@ public class ShapeTest {
 	    // Checking if circle area is correct
 	    assertEquals("Area calculation for Circle is incorrect", Math.PI * 4, circle.area(), 0.001);
 	}
-
+	@Test
+	public void testRhombusArea() {
+	    //  rhombus to check  area calculation
+	    Rhombus rhombus = new Rhombus("Rhombus", 4.0, 3.0);
+	    // Testing for area calculation if its correct
+	    assertEquals("Area calculation for Rhombus is incorrect", 6.0, rhombus.area(), 0.001);
+	}
+	
 }
