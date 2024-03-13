ee077768d0c4e51e8fa9a80662685bab7619533c
regazo <z.mbame11@gmail.com>
2024-03-13 23:45:28 +0000

    added unit tests for circles area

--- WorksheetThree_3074880/src/test/java/Shapes/ShapeTest.java
+++ WorksheetThree_3074880/src/test/java/Shapes/ShapeTest.java
@@ -1,9 +1,18 @@
 package Shapes;
 //Zakarea Erezzaghi 3074880
 
+
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class ShapeTest {
-    // We will add tests ltr
+
+	@Test
+	public void testCircleArea() {
+	    // circle with radius 2
+	    Circle circle = new Circle("Circle", 2.0);
+	    // Checking if circle area is correct
+	    assertEquals("Area calculation for Circle is incorrect", Math.PI * 4, circle.area(), 0.001);
+	}
+
 }
